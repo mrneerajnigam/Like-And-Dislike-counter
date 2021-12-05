b7f5cc6cbbf055c2aa224707e3385300e9afeed4
Neeraj Nigam <888neerajnigam@gmail.com>
2021-12-05 12:57:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// function for like Button
+
+
+function like(){     
+        let l = document.getElementById('like_reciever');
+        l.value = parseInt(l.value)  + 1;
+        };
+
+function dislike(){    
+        let dl = document.getElementById('dislike_reciever');
+        dl.value =parseInt(dl.value)  +1;
+        }
+    
